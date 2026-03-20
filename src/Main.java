3f4e6086037041d222eb322b29c2f90a365117f4
bishal-0x01 <learndesignandalgorithm@gmail.com>
2026-03-21 02:11:18 +0530

    Added Required methods

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         BufferedWriter bw;
 
         if (System.getProperty("ONLINE_JUDGE") == null) {
-            br = new BufferedReader(new FileReader("Input-Output-Folder/input.txt"));
-            bw = new BufferedWriter(new FileWriter("Input-Output-Folder/output.txt"));
+            br = new BufferedReader(new FileReader("input.txt"));
+            bw = new BufferedWriter(new FileWriter("output.txt"));
         } else {
             br = new BufferedReader(new InputStreamReader(System.in));
             bw = new BufferedWriter(new OutputStreamWriter(System.out));
