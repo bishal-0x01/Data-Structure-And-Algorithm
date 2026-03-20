3dcf58cc99263efa0125af55ca888d2165ae6496
bishal-0x01 <learndesignandalgorithm@gmail.com>
2026-03-21 03:02:16 +0530

    Added Question01

--- Question-Practice-List/Hashing/Question01/Question01.java
+++ Question-Practice-List/Hashing/Question01/Question01.java
@@ -1,3 +1,24 @@
+/*
+
+We are given an Array of Numbers. We have to find and print any Number with Maximum Frequency and Minimum Frequency.
+
+Example Testcase:-
+
+Arr = [3, 2, 3, 2, 4, 3]
+
+Frequencies of Elements of Array are:-
+
+3 - 3
+2 - 2
+4 - 1
+
+Maximum Frequency:- Element is 3, Frequency is 3
+Minimum Frequency:- Element is 4, Frequency is 1
+
+*/
+
+
+
 import java.io.*;
 import java.util.*;
 
