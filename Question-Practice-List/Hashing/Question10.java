c40dabc74e40bc818bc8d82c17f503976ca5bdd5
bishal-0x01 <learndesignandalgorithm@gmail.com>
2026-03-21 11:54:59 +0530

    q10

--- Question-Practice-List/Hashing/Question10.java
+++ Question-Practice-List/Hashing/Question10.java
@@ -1,3 +1,22 @@
+/*
+
+William Macfarlane wants to look at an array.
+
+You are given a list of N numbers and Q queries. Each query is specified by two numbers i and j; the answer to each query is the sum of every number between the range [i, j] (inclusive).
+
+Note: the query ranges are specified using 0-based indexing.
+
+Input
+The first line contains N, the number of integers in our list (N ≤ 100,000). The next line holds N numbers that are guaranteed to fit inside an integer. Following the list is a number Q (Q ≤ 10,000). The next Q lines each contain two numbers i and j which specify a query you must answer (0 ≤ i, j ≤ N-1).
+
+
+Link : https://www.spoj.com/problems/CSUMQ/
+
+*/
+
+
+
+
 import java.io.*;
 import java.util.*;
 
